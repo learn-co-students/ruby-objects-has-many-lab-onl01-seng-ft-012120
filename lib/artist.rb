f35cef0ddfc28e814c8b2357ae5400d34b5bfb6e
Kyle McKenna <mckennakyler@gmail.com>
2020-02-03 23:13:36 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist 
+  attr_accessor :name 
+  def initialize(name)  
+    @name = name 
+  end
+  
+  def song 
+    
+  end 
+  
+  def add_song(song)
+    
+  end 
+  
+  def add_song_by_name(name)
+  
+  end 
+  
+  def self.song_count 
+    
+  end   
+  
+end 

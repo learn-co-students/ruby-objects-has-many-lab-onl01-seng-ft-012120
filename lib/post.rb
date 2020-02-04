e23243d6aa18b4c2be3647800d664eef32a5ee4e
Harmandeep S. Mand <harmandeepmand.hm@gmail.com>
2020-02-03 20:49:17 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,24 @@
+require 'pry'
+class Post
+
+    attr_accessor :title, :author
+    @@all = []
+    def initialize(title)
+      @title = title
+      @@all << title
+      @@all
+    end
+
+    def self.all
+      @@all
+        
+    end
+  
+    def author_name
+      if @author == nil
+        return nil
+      else
+        @author.name
+      end
+    end
+  end

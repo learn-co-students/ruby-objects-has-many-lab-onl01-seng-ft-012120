f542b78bee5630d9ef774126f4692caf64e0ba64
Kyle McKenna <mckennakyler@gmail.com>
2020-02-06 05:25:25 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,30 @@
+require 'pry'
+
 class Artist 
+  
   attr_accessor :name 
+  
+  @@songs = []
+  
   def initialize(name)  
     @name = name 
   end
   
-  def song 
-    
+  def songs # go to song.rb 
+    Song.all 
   end 
-  
+   
   def add_song(song)
-    
+    song.artist = self 
+    @@songs << song 
   end 
   
   def add_song_by_name(name)
-  
+    add_song(Song.new(name))
   end 
   
   def self.song_count 
-    
+    Song.all.length 
   end   
   
 end 
--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,30 @@
-class Author 
+require 'pry'
+
+class Author  
   
-end
+  attr_accessor :name 
+  
+  @@posts = []
+  
+  def initialize(name)  
+    @name = name 
+  end
+  
+  def posts  
+    Post.all 
+  end 
+   
+  def add_post(title)
+    title.author = self 
+    @@posts << title 
+  end 
+  
+  def add_post_by_title(title)
+    add_post(Post.new(title))
+  end 
+  
+  def self.post_count 
+    Post.all.length 
+  end   
+  
+end 
